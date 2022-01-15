2cb7b141b59b71903448d3cbb051b04d462a5f0d
Leonardo Costa <leosilva.sjc@gmail.com>
2022-01-15 15:25:57 -0300

    Finalizando implementações e realizando testes

--- src/main/java/one/digitalInnovation/ArvoreBinaria.java
+++ src/main/java/one/digitalInnovation/ArvoreBinaria.java
@@ -19,9 +19,8 @@ public class ArvoreBinaria <T extends Comparable<T>>{
         }else if(novoNO.getConteudo().compareTo(atual.getConteudo())<0){
             //caso o nó atual nao seja null,
             // são comparados os dois nós para determinar se a sua posição esta a esquerda ou a direita
-
-            //enquanto nao achar uma posição de elemento null
-            // a chamada recursiva ocorre de novo e novo até acha a posição null
+            //enquanto não achar uma posição de elemento null
+            // a chamada recursiva ocorre de novo e novo até achar uma posição null
             atual.setNoEsq(inserir(atual.getNoEsq(),novoNO));
 
         }else{
@@ -29,4 +28,131 @@ public class ArvoreBinaria <T extends Comparable<T>>{
         }
         return atual;
     }
+
+    //________________________________________________________________________________________
+    public void remover(T conteudo){
+        try {
+            BinNO<T> atual = this.raiz;
+            BinNO<T> pai = null;
+            BinNO<T> filho;
+            BinNO<T> temp;
+
+            while (atual != null && !atual.getConteudo().equals(conteudo)){
+                pai = atual;
+                if(conteudo.compareTo(atual.getConteudo())<0){
+                    atual = atual.getNoEsq();
+                }else{
+                    atual = atual.getNoDir();
+                }
+            }
+            if (atual == null){
+                System.out.println("conteudo não existe");
+            }
+
+            assert atual != null;
+            if(pai == null){
+                if(atual.getNoDir() == null){
+                    this.raiz = atual.getNoEsq();
+                }else if (atual.getNoEsq() == null){
+                    this.raiz = atual.getNoDir();
+                }else{
+                    for(temp = atual, filho = atual.getNoEsq();
+                        filho.getNoDir() != null;
+                        temp = filho, filho = filho.getNoEsq()
+                    ){
+                        if(filho != atual.getNoEsq()){
+                            temp.setNoDir(filho.getNoEsq());
+                            filho.setNoEsq(raiz.getNoEsq());
+                        }
+                    }
+                    filho.setNoDir(raiz.getNoDir());
+                    raiz = filho;
+
+                }
+
+            }else {
+                if(atual.getNoDir() == null){
+                    if(pai.getNoDir() == atual ){
+                        pai.setNoEsq(atual.getNoEsq());
+                    }else{
+                        pai.setNoDir(atual.getNoEsq());
+                    }
+
+                }else if(atual.getNoEsq() == null){
+                    if(pai.getNoEsq() == null){
+                        pai.setNoEsq(atual.getNoDir());
+                    }else{
+                        pai.setNoDir(atual.getNoDir());
+                    }
+                }else{
+                    for(
+                            temp = atual, filho = atual.getNoEsq();
+                            filho.getNoDir() != null;
+                            temp = filho, filho = filho.getNoDir()
+                    ){
+
+                        if(filho != atual.getNoEsq()){
+                            temp.setNoDir(filho.getNoEsq());
+                            filho.setNoEsq(atual.getNoEsq());
+                        }
+                        filho.setNoDir(atual.getNoDir());
+                        if(pai.getNoEsq() == atual){
+                            pai.setNoEsq(filho);
+                        }else{
+                            pai.setNoDir(filho);
+                        }
+                    }
+
+                }
+            }
+
+        }catch (NullPointerException erro){
+            System.out.println("Conteudo não encontrado");
+        }
+    }
+
+    //________________________________________________________________________________________
+
+    public void inOrdem(){
+        System.out.println("\nExibindo inOrdem");
+        inOrdem(this.raiz);
+    }
+
+    private void inOrdem(BinNO<T> raiz) {
+        if(raiz != null){
+            inOrdem(raiz.getNoEsq());
+            System.out.print(raiz.getConteudo() + ",");
+            inOrdem(raiz.getNoDir());
+        }
+    }
+
+    //________________________________________________________________________________________
+    public void posOrdem(){
+        System.out.println("\nExibindo posOrdem");
+        posOrdem(this.raiz);
+    }
+
+    private void posOrdem(BinNO<T> raiz) {
+        if(raiz != null){
+            posOrdem(raiz.getNoEsq());
+            posOrdem(raiz.getNoDir());
+            System.out.print(raiz.getConteudo() + ",");
+        }
+    }
+
+    //________________________________________________________________________________________
+    public void preOrdem(){
+        System.out.println("\nExibindo preOrdem");
+        preOrdem(this.raiz);
+    }
+
+    private void preOrdem(BinNO<T> raiz) {
+        if(raiz != null){
+            System.out.print(raiz.getConteudo() + ",");
+            preOrdem(raiz.getNoEsq());
+            preOrdem(raiz.getNoDir());
+        }
+    }
+
+
 }
